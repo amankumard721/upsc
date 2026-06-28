3346cffc8b13b55ea1a12ad79ddf15293bfce686
UPSC Aspirant <aspirant@prepai.in>
2026-06-28 21:37:18 +0530

    Fix: Overrode MainActivity.java to natively disable overscroll bounce and vertical scrollbars, and updated CSS for mobile touch device compatibility

--- android/app/src/main/java/com/prepai/app/MainActivity.java
+++ android/app/src/main/java/com/prepai/app/MainActivity.java
@@ -1,5 +1,21 @@
 package com.prepai.app;
 
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        // Disable Android WebView overscroll bounce effect and native scrollbars
+        WebView webView = this.bridge.getWebView();
+        if (webView != null) {
+            webView.setOverScrollMode(View.OVER_SCROLL_NEVER);
+            webView.setVerticalScrollBarEnabled(false);
+            webView.setHorizontalScrollBarEnabled(false);
+        }
+    }
+}
