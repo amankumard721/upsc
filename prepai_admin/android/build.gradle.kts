6a453bcefaee7ddc2f514647c5dc45d7d9b1f09e
UPSC Aspirant <aspirant@prepai.in>
2026-06-30 13:53:22 +0530

    fix: force compileSdk to 36 for all subprojects in root gradle configuration to resolve metadata check conflicts

--- prepai_admin/android/build.gradle.kts
+++ prepai_admin/android/build.gradle.kts
@@ -1,3 +1,13 @@
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.2.1")
+    }
+}
+
 allprojects {
     repositories {
         google()
@@ -15,8 +25,18 @@ subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
+
 subprojects {
     project.evaluationDependsOn(":app")
+}
+
+subprojects {
+    afterEvaluate {
+        if (plugins.hasPlugin("com.android.application") || plugins.hasPlugin("com.android.library")) {
+            val android = extensions.findByName("android") as? com.android.build.gradle.BaseExtension
+            android?.compileSdkVersion(36)
+        }
+    }
 }
 
 tasks.register<Delete>("clean") {
