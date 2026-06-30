d37ed86fc675087cf342f1191a333516dd92a0b7
UPSC Aspirant <aspirant@prepai.in>
2026-06-30 14:57:18 +0530

    fix: import crypto package in book_lessons_screen.dart for AWS SigV4 signer

--- prepai_admin/lib/screens/book_lessons_screen.dart
+++ prepai_admin/lib/screens/book_lessons_screen.dart
@@ -10,6 +10,7 @@ import 'package:path_provider/path_provider.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:just_audio/just_audio.dart';
 import 'package:http/http.dart' as http;
+import 'package:crypto/crypto.dart';
 import '../models/models.dart';
 import '../providers/admin_state.dart';
 import 'data_manager.dart';
