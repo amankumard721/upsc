7ab96c91665c0b9458d9c2f7c907d5a6172619d3
UPSC Aspirant <aspirant@prepai.in>
2026-06-30 13:45:01 +0530

    fix: set Android compileSdk to 36 to support modern plugins

--- prepai_admin/android/app/build.gradle.kts
+++ prepai_admin/android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.prepai.admin.prepai_admin"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 36
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
